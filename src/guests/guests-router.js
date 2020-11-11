36499fb30a458052dd0d57ece4fc56d2a201a9ab
Vendy Prum <vendy@thinkful.com>
2020-11-11 11:01:28 -0800

    post to get user info with family

--- src/guests/guests-router.js
+++ src/guests/guests-router.js
@@ -1,27 +1,76 @@
-const express = require('express')
-const path = require('path')
-const GuestsService = require('./guests-service')
-const guestRouter = express.Router()
-const jsonBodyParser = express.json()
-const serializeGuests = (guest)=>{
-    return {firstname:guest.firstname, lastname: guest.lastname}
-}
-guestRouter
-    .route('/')
-    .post(jsonBodyParser,(req,res,next)=>{
-        const {firstname, lastname} = req.body;
-        //need to check capitalization of name from database and input 
-        GuestsService.isOnGuestList(req.app.get('db'), firstname, lastname)
-            .then(name=>{
-                console.log(name)
-                res.json(name.map(serializeGuests))
-            })
-            .then(data=>{
-                res.status(201).json(data)
-            })
-            .catch(next)
+const express = require("express");
+const path = require("path");
+const { getFamily } = require("./guests-service");
+const GuestsService = require("./guests-service");
+const guestRouter = express.Router();
+const jsonBodyParser = express.json();
+const serializeGuests = (guest) => {
+  return {
+    firstname: guest.firstname,
+    lastname: guest.lastname,
+    plus_one: guest.plus_one,
+    family: guest.family,
+    note: guest.note,
+  };
+};
+guestRouter.route("/").post(jsonBodyParser, (req, res, next) => {
+  const { firstname, lastname } = req.body;
+  const knex = req.app.get("db");
+  //need to check capitalization of name from database and input
+  GuestsService.isOnGuestList(knex, firstname, lastname)
+    .then((name) => {
+      console.log(name)
+      //if no group just return
+      if (!name[0].family) {
+        res.status(200).json(name);
+      }
+      //if in group get a family
+      else {
+        getFamily(knex, name[0].family)
+          .then((data) => {
+            res.status(200).json(data);
+          })
+          .catch(next);
+      }
     })
-    
-
+    .catch(next);
+});
 
 module.exports = guestRouter;
+
+// if we found the person and they're in a group
+// const response = {
+//   note: 'oaisjdoiajsd',
+//   people: [
+//     {
+//       id: 1,
+//       firstname: "Charlie",
+//       lastname: "Prum",
+//       response: null,
+//     },
+//     {
+//       id: 2,
+//       firstname: "Something",
+//       lastname: "Prum",
+//       response: null,
+//     },
+//     {
+//       id: 3,
+//       firstname: "a",
+//       lastname: "b",
+//       response: null,
+//     },
+//   ],
+// };
+
+// const responsePlusOne = {
+//   allow_plus_one: true,
+//   people: [
+//     {
+//       id: 1,
+//       firstname: "Charlie",
+//       lastname: "Prum",
+//       response: null,
+//     },
+//   ],
+// };
--- src/guests/guests-service.js
+++ src/guests/guests-service.js
@@ -1,11 +1,18 @@
 const GuestsService = {
     isOnGuestList(db, firstname, lastname){
         return db('wedding')
-            .select('firstname', 'lastname')
+            .select('*')
             .from('guests')
             .where({'firstname': firstname, 'lastname': lastname})
 
+    },
+    getFamily(db, family){
+        return db('wedding')
+            .select('*')
+            .from('guests')
+            .where({"family":family})
     }
+    
 }
 
 module.exports = GuestsService;
